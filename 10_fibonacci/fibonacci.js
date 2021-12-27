0ce4967092e191feb776bf7804cf97b9882914a9
Misha Oleinychenko <m.oleinychenko@gmail.com>
2021-12-27 13:01:50 -0500

    added solutions

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (num < 0) return 'OOPS'
+    let first = 0;
+    let second = 1;
+    let tempStorage;
+    const fibonacci = [0, 1];
+    for (let index = 0; index < num; index++) {
+        tempStorage = first + second;
+        fibonacci.push(tempStorage);
+        first = second;
+        second = tempStorage;
+    }
+    return fibonacci[num];
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    let today = new Date().getFullYear()
+    let oldest = {};
+    people.forEach((person) => {
+        if (!Object.keys(oldest).length) {
+            oldest = { ...person };
+        } else if (Object.keys(oldest).length) {
+            if (
+                (oldest.yearOfDeath ?? today) - oldest.yearOfBirth >
+                (person.yearOfDeath ?? today) - person.yearOfBirth
+            ) return
+            oldest = person;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
